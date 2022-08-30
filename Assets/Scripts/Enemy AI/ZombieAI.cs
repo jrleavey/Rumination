d602adc7e6fd4e6f1286c3db460768c8802d6b7f
jack leavey <jrleavey@gmail.com>
2022-08-30 13:37:03 -0400

    Basic Zombie AI, follows player, navmeshed

--- Assets/Scripts/Enemy AI/ZombieAI.cs
+++ Assets/Scripts/Enemy AI/ZombieAI.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieAI : MonoBehaviour
 {
+    private NavMeshAgent _navMeshAgent;
+
+    [SerializeField]
+    private GameObject _player;
     void Start()
     {
-        
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _player = GameObject.Find("Player");
     }
 
     void Update()
     {
-        
+        Movement();
     }
+    private void Movement()
+    {
+        _navMeshAgent.destination = _player.transform.position;
+    }
+
+    private void Attack()
+    {
+
+    }
+
 }
