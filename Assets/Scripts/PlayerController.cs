78018f17fbb0b9e16c75203f926df2286b21a6f6
jack leavey <jrleavey@gmail.com>
2022-09-12 13:56:28 -0400

    Raycasting UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -118,6 +118,7 @@ public class PlayerController : MonoBehaviour
            if (Physics.Raycast(RaycastHolder.transform.position, RaycastHolder.transform.forward, out hit, range))
             {
                 Debug.Log(hit.transform);
+                Debug.DrawRay(RaycastHolder.transform.position, RaycastHolder.transform.forward, Color.red);
             }                                         
         }
     }
