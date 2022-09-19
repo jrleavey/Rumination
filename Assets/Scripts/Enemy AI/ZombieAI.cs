d5fa0b9c4664e8d2c535289f3e45dc5c0efac0c5
jack leavey <jrleavey@gmail.com>
2022-09-19 11:19:40 -0400

    zombie AI

--- Assets/Scripts/Enemy AI/ZombieAI.cs
+++ Assets/Scripts/Enemy AI/ZombieAI.cs
@@ -9,8 +9,8 @@ public class ZombieAI : MonoBehaviour
     private enum AIState
     {
         Passive,
-        Investigating,
-        Hostile
+        Hostile,
+        Dying
     }
     [SerializeField]
     private AIState _AIState;
@@ -19,28 +19,78 @@ public class ZombieAI : MonoBehaviour
     public float angle = 90f;
     public bool _isChasingPlayer = false;
     public GameObject _player;
+    [SerializeField]
     private bool _IAmWaiting;
+    [SerializeField]
+    private AudioSource _audioSource;
+    [SerializeField]
+    private AudioClip _roar;
+    [SerializeField]
+    private AudioClip _onHit;
+    [SerializeField]
+    private AudioClip _attack;
+    [SerializeField]
+    private AudioClip _onDeath;
+
+    private Animator _anim;
+
+    public LayerMask targetMask;
+    public LayerMask ObstructionMask;
+    public bool canSeePlayer;
+    private bool isDying = false;
+    [SerializeField]
+    private bool haveIScreamed = false;
+    [SerializeField]
+    private GameObject[] _hitboxes;
+
+
+    private int _maxhp = 6;
+    [SerializeField]
+    private int _currentHp;
 
     [Range(0, 500)] public float walkRadius;
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
         _player = GameObject.Find("Player");
+        StartCoroutine(CheckForPlayer());
+        _currentHp = Random.Range(1, 6);
     }
 
     void Update()
     {
-        switch(_AIState)
+
+        switch (_AIState)
         {
             case AIState.Passive:
                 Wander();
                 break;
-            case AIState.Investigating:
-                // Investigating
-                break;
             case AIState.Hostile:
                 ChasePlayer();
                 break;
+            case AIState.Dying:
+                Die();
+                break;
+        }
+
+        if (canSeePlayer == true)
+        {
+            StartCoroutine(Roar());
+            _AIState = AIState.Hostile;
+        }
+        if (_currentHp <= 0)
+        {
+            _AIState = AIState.Dying;
+        }
+        if (_navMeshAgent.speed == 0)
+        {
+            _anim.SetBool("isMoving", false);
+        }
+        else
+        {
+            _anim.SetBool("isMoving", true);
+
         }
     }
 
@@ -51,14 +101,70 @@ public class ZombieAI : MonoBehaviour
             _navMeshAgent.SetDestination(RandomNavMeshLocation());
             _IAmWaiting = true;
             StartCoroutine(RandomWaitTimer());
+        }
+    }
+    private IEnumerator Roar()
+    {
+        _navMeshAgent.speed = 0;
 
+        if (_audioSource.isActiveAndEnabled == false)
+        {
+            _audioSource.enabled = true;
+        }
+        _anim.SetTrigger("hasSeenPlayer");
+        yield return new WaitForSeconds(2f);
+        _navMeshAgent.speed = 5;
+        _anim.SetBool("haveIScreamed", true);
+
+    }
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, ObstructionMask))
+                {
+                    canSeePlayer = true;
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
+    }
+    private IEnumerator CheckForPlayer()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
         }
     }
     IEnumerator RandomWaitTimer()
     {
-        
-        int wait_time = Random.Range(1, 4);
+        int wait_time = Random.Range(3, 7);
+        _navMeshAgent.speed = 0;
         yield return new WaitForSeconds(wait_time);
+        _navMeshAgent.speed = 5;
         print("I waited for " + wait_time + "sec");
         _IAmWaiting = false;
     }
@@ -77,12 +183,42 @@ public class ZombieAI : MonoBehaviour
     private void ChasePlayer()
     {
         _isChasingPlayer = true;
+        StartCoroutine(Roar());
         _navMeshAgent.destination = _player.transform.position;
-    }
 
-    private void Attack()
+        if (_navMeshAgent.remainingDistance < 1)
+        {
+            _anim.SetBool("inRangetoAttack", true);
+            _hitboxes[0].SetActive(true);
+            _hitboxes[1].SetActive(true);
+
+        }
+        else
+        {
+            _hitboxes[0].SetActive(false);
+            _hitboxes[1].SetActive(false);
+            _anim.SetBool("inRangetoAttack", false);
+
+        }
+    }
+    private void Damage()
     {
-        _player.GetComponent<PlayerController>().TookDamage(1);
+        _currentHp--;
+        canSeePlayer = true;
     }
 
+    private void Die()
+    {
+        if (_currentHp <= 0)
+        {
+            _anim.SetTrigger("isDying");
+            _navMeshAgent.enabled = false;
+            StartCoroutine(DespawnTimer());
+        }
+    }
+    private IEnumerator DespawnTimer()
+    {
+        yield return new WaitForSeconds(4f);
+        Destroy(this.gameObject);
+    }
 }
